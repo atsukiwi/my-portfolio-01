b4c489d19ac2ebaf403f4f7f22fded0b9c4891ee
atsukiwi <atsuki.hattori@daedalus.co.jp>
2024-09-11 02:54:21 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // デプロイする場所に応じて変更が必要かもしれません
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    // 必要に応じて他のビルド設定を追加
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
